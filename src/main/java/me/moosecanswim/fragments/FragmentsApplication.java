351e9455e0dce6d512273387766b6adae52d1fdb
moosecanswim <kyle.gallagher.09@gmail.com>
2017-08-17 09:30:50 -0400

    Initial commit

---
+++ src/main/java/me/moosecanswim/fragments/FragmentsApplication.java
@@ -0,0 +1,12 @@
+package me.moosecanswim.fragments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FragmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FragmentsApplication.class, args);
+	}
+}
